bf51aab181e7c1904e09585fadde4803fea1002c
IvanTaborkikh <62793444+IvanTaborkikh@users.noreply.github.com>
2020-04-04 12:09:33 +0300

    0.0
    start

---
+++ 2d game test/Assets/PlayerMuvement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMuvement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runspeed = 40f;
+    bool jump = false;
+    bool Crouch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("jumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            Crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            Crouch = false;
+        }
+    }
+   public void Onlanding()
+    {
+        animator.SetBool("jumping", false);
+    }
+    public void Oncroaching (bool croaching)
+    {
+        animator.SetBool("crouching", croaching);
+    }
+     void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, Crouch, jump);
+        jump = false;
+        
+    }
+}
